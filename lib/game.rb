05f7b3c14fa93f90a95c8636c20e4f53f2f29acb
Paula Muldoon <pmuldoon@umich.edu>
2017-09-06 16:09:23 -0500

    added ask_name method

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,12 @@
 class Game
 
-  def step0
-    print 'While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.'
-  end
+def ask_name
+  name = $stdin
+  print 'Your name is ' + name + '.'
+end
+
+def step0
+  print 'While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.'
+end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,34 @@
 require 'game'
 
 describe Game do
-  before do
-    $stdin = 0
+
+  context 'setup' do
+    before do
+      $stdin = 'Patrick'
+    end
+
+    after do
+      $stdin = STDIN
+    end
+
+    it 'should ask the user name' do
+      game = Game.new
+      expect{game.ask_name}.to output('Your name is Patrick.').to_stdout
+    end
   end
 
-  after do
-    $stdin = STDIN
-  end
-  it 'should ask be able to choose to go home' do
-    game = Game.new
-    expect{game.step0}.to output('While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.').to_stdout
+  context 'steps' do
+    before do
+      $stdin = 0
+    end
+
+    after do
+      $stdin = STDIN
+    end
+
+    it 'should ask be able to choose to go home' do
+      game = Game.new
+      expect{game.step0}.to output('While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.').to_stdout
+    end
   end
 end
