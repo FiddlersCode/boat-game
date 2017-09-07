960221e0b68029308c9a782637fae8d3c9306acb
Michael Palladino <FionaPalladino@Michaels-MacBook-Air.local>
2017-09-07 11:06:44 -0400

    modified answer1

--- lib/game.rb
+++ lib/game.rb
@@ -3,41 +3,47 @@ class Game
     puts 'What is your name?'
     name = gets.chomp
     puts 'Your name is ' + name + '.'
+    self.question0
   end
 
   def question0
     puts 'There is a storm.  Would you like to go home or stay on the water and try to get to Beaver Island for your party?'
     puts 'Please press 0 or 1.'
     @answer = gets.chomp
+    self.answer0
   end
 
   def answer0
     if @answer == '0'
       puts 'While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.'
+      self.question1
     elsif @answer == '1'
       puts 'You go to the party on Beaver Island and break your legs dancing.'
     else
       puts 'Please answer 0 or 1.'
+      self.question0
     end
   end
 
   def question1
-    puts 'the robber had accidentally left one credit card, would you like to go after the robber or try to replace all the stolen stuff?'
+    puts 'The robber accidentally left one credit card, would you like to go after the robber or try to replace all the stolen stuff?'
     @answer = gets.chomp
+    self.answer1
   end
 
   def answer1
     if @answer == '0'
-      puts 'this'
+      puts 'You chase the robber across Lake Michigan and you see his boat and so you look
+      through it to see if you can find your stuff, but you unfortunately don\'t find the stuff.'
+      puts 'You finally find the robber and your stuff and it turns out that the robber was your brother who had lost all his money in a bet and decided to be a robber without telling anybody. You turn him in and you get all of your stuff back.'
     elsif @answer == '1'
-      puts 'that'
+      puts
     else
       puts 'Please answer 0 or 1.'
+      self.question1
     end
   end
 end
 
 @game = Game.new
 @game.ask_name
-@game.question0
-@game.answer0
