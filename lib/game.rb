32a0a834c1146c6a588360a0a2a7dbe462496ac2
Paula Muldoon <pmuldoon@umich.edu>
2017-09-07 19:15:13 -0500

    all tests now passing

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,39 @@
 class Game
+
+  attr_reader :name
+
+  def play_game
+    self.ask_name
+    self.get_name
+    self.show_name
+    self.ask_question0
+    self.answer_question0
+    self.answer0
+    self.question1
+  end
+
   def ask_name
     puts 'What is your name?'
-    name = gets.chomp
-    puts 'Your name is ' + name + '.'
   end
 
-  def question0
+  def get_name
+    @name = $stdin.gets.chomp
+  end
+
+  def show_name
+    puts 'Your name is ' + @name + '.'
+  end
+
+  def ask_question0
     puts 'There is a storm.  Would you like to go home or stay on the water and try to get to Beaver Island for your party?'
     puts 'Please press 0 or 1.'
-    @answer = gets.chomp
   end
 
-  def answer0
+  def get_answer0
+    @answer = $stdin.gets.chomp
+  end
+
+  def show_answer0
     if @answer == '0'
       puts 'While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.'
     elsif @answer == '1'
@@ -36,8 +58,3 @@ class Game
     end
   end
 end
-
-@game = Game.new
-@game.ask_name
-@game.question0
-@game.answer0
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,30 +1,54 @@
-# require 'game'
-#
-# describe Game do
-#   before(:each) do
-#     @game = Game.new
-#   end
-#
-#   context 'setup' do
-#     it 'should ask the user name' do
-#       $stdin = 'Patrick'
-#       expect{@game.ask_name}.to output('Your name is Patrick.').to_stdout
-#     end
-#   end
-#
-#   context 'step 0' do
-#     it 'should give context' do
-#       expect{@game.question0}.to output('There is a storm.  Would you like to go home or stay on the water and try to get to Beaver Island for your party?').to_stdout
-#     end
-#
-#     it 'should ask be able to choose to go home' do
-#       $stdin = 0
-#       expect{@game.answer0}.to output('While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.').to_stdout
-#     end
-#
-#     it 'should be able to choose to go to Beaver Island' do
-#       $stdin = 1
-#       expect{@game.answer0}.to output('You go to the party on Beaver Island and break your legs dancing.').to_stdout
-#     end
-#   end
-# end
+require 'game'
+require 'stringio'
+
+describe Game do
+  before(:each) do
+    @game = Game.new
+  end
+
+  context 'setup' do
+    before do
+      $stdin = StringIO.new("Patrick\n")
+    end
+
+    after do
+      $stdin = STDIN
+    end
+
+    it 'should ask the user name' do
+      @game.get_name
+      expect{@game.show_name}.to output("Your name is Patrick.\n").to_stdout
+    end
+  end
+
+  context 'step 0' do
+    before do
+      $stdin = StringIO.new("0\n")
+    end
+
+    after do
+      $stdin = STDIN
+    end
+
+    it 'should ask be able to choose to go home' do
+      @game.get_answer0
+      expect{@game.show_answer0}.to output("While you were gone, you were robbed! The robber had actually sailed right past you to go to the shore that you did not go to.\n").to_stdout
+    end
+
+    context 'step 0-1' do
+
+      before do
+        $stdin = StringIO.new("1\n")
+      end
+
+      after do
+        $stdin = STDIN
+      end
+
+      it 'should be able to choose to go to Beaver Island' do
+        @game.get_answer0
+        expect{@game.show_answer0}.to output("You go to the party on Beaver Island and break your legs dancing.\n").to_stdout
+      end
+    end
+  end
+end
